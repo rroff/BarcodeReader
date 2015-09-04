3949a5ee4b81a6e4129a8aa09a31728f5d465c01
Ron Roff <rroff@roff.us>
2015-09-03 22:31:48 -0400

    Original proof of concept, from Google Code Lab

--- app/src/main/java/rroff/roff/us/barcodereader/MainActivity.java
+++ app/src/main/java/rroff/roff/us/barcodereader/MainActivity.java
@@ -1,16 +1,84 @@
+/*
+ * Copyright(c) 2015 Ron Roff
+ * All Rights Reserved.
+ *
+ * Author: Ron Roff (rroff@roff.us)
+ * Creation Date: 9/3/2015
+ */
 package rroff.roff.us.barcodereader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.SparseArray;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends AppCompatActivity {
+
+    BarcodeDetector mDetector;
+
+    ViewHolder mHolder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mHolder = new ViewHolder();
+        mHolder.processBtn = (Button)findViewById(R.id.button);
+        mHolder.processBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO
+            }
+        });
+        mHolder.barcodeIv = (ImageView)findViewById(R.id.imgview);
+        mHolder.outputTv = (TextView)findViewById(R.id.txtContent);
+
+        mDetector = new BarcodeDetector.Builder(getApplicationContext())
+                        .setBarcodeFormats(Barcode.DATA_MATRIX | Barcode.QR_CODE)
+                        .build();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // TODO: Replace static barcode with captured image
+        Bitmap bitmap = BitmapFactory.decodeResource(
+                getApplicationContext().getResources(),
+                R.drawable.puppy);
+        processBarcode(bitmap);
+    }
+
+    private void processBarcode(Bitmap bitmap) {
+        mHolder.barcodeIv.setImageBitmap(bitmap);
+
+        // NOTE from Google Code Lab: It’s possible that, the first time our barcode detector runs,
+        // Google Play Services won’t be ready to process barcodes yet.  So we need to check if our
+        // detector is operational before we use it.  If it isn’t, we may have to wait for a
+        // download to complete, or let our users know that they need to find an internet
+        // connection or clear some space on their device.
+        if(!mDetector.isOperational()){
+            mHolder.outputTv.setText("Could not set up the detector!");
+            return;
+        }
+
+        Frame frame = new Frame.Builder().setBitmap(bitmap).build();
+        SparseArray<Barcode> barcodes = mDetector.detect(frame);
+
+        Barcode thisCode = barcodes.valueAt(0);
+        mHolder.outputTv.setText(thisCode.rawValue);
     }
 
     @Override
@@ -33,5 +101,11 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ViewHolder {
+        public Button processBtn;
+        public ImageView barcodeIv;
+        public TextView outputTv;
     }
 }
